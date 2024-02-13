2cb240c9894b9e3022fc5becaa14571a78ddf21b
UtsavBrahmakshatriya <brahmakshatriyautsav@gmail.com>
2024-02-13 23:07:19 +0530

    Made minor fixes in LLM usage and the way of showing the conversation history

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import streamlit as st
 from dotenv import load_dotenv
 from pypdf import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain_community.vectorstores import chroma
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain_community.chat_models import ChatOpenAI
+from langchain_community.llms import huggingface_hub
+
 from datetime import datetime
 
 from htmltemplates import css, bot_template, user_template
@@ -30,15 +31,16 @@ def get_text_chunks(raw_text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    # embeddings = OpenAIEmbeddings()
+    chroma_db = chroma.Chroma()
     embeddings = HuggingFaceInstructEmbeddings(model_name= "hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts= text_chunks, embedding= embeddings)
+    vectorstore = chroma_db.from_texts(texts=text_chunks,embedding=embeddings,persist_directory="./data/vectordb/")
     return vectorstore
 
 def get_conversation_chain(vectorstore):
+    llm = huggingface_hub.HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(),
+        llm=llm,
         retriever=vectorstore.as_retriever(),
         memory= memory
     )
@@ -48,11 +50,11 @@ def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response["chat_history"]
 
-    for i, message in enumerate(st.session_state.chat_history):
+    for i, message in enumerate(reversed(st.session_state.chat_history)):
         if i%2==0:
-            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
-        else:
             st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+        else:
+            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
 
 def main():
     load_dotenv()
@@ -64,16 +66,16 @@ def main():
         st.session_state.chat_history = None
 
     st.header("Private ChatGPT :lock:")
+    container = st.container()
     user_question = st.text_input("Ask me anything")
-
-    if user_question:
-        handle_userinput(user_question)
-
+    if st.button("Find Answer"):
+        with st.spinner("Finding..."):
+            handle_userinput(user_question)
 
     with st.sidebar:
         st.header("Admin Zone")
         st.subheader("Your Documents")
-        pdf_docs = st.file_uploader("Upload your PDFs here", accept_multiple_files=True)
+        pdf_docs = st.file_uploader("Upload your PDFs here", accept_multiple_files=True,type="PDF")
         if st.button("Process"):
             with st.spinner("Processing..."):
                 # get pdf text
